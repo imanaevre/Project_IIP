7db54ec45f2ca8df473ec8fe7b236ab07ab6687c
imanaevre <imanaev.re@icloud.com>
2026-05-10 15:14:48 +0300

    добавил функцию для среднего значения по всем дням и функцию для анализа дней недели

--- analysis.py
+++ analysis.py
@@ -8,5 +8,15 @@ def record_date(top, df):
 def antirecord_date(top, df):
     return df.nsmallest(top, 'steps')
 
+def mean_steps(df):
+    return df['steps'].mean().astype(int)
 
+
+def an_days_of_week(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df['days'] = df['date'].dt.dayofweek
+    d_of_week = df.groupby('days')['steps'].mean().astype(int)
+    return max(d_of_week), min(d_of_week), d_of_week
+
+print(mean_steps(df))
 
